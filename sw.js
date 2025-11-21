d84f66420175ce68f5c4dedaba8a3c12c2b5aa24
Lutan <lutancorpinfoteam@gmail.com>
2025-11-21 06:02:02 +0300

    ads add

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10216427
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10216450
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
